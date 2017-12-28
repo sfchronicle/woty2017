7178597fce53dff598cc1f77701c0127500989b1
Lucio Villa <lucio310@gmail.com>
2017-12-28 12:26:48 -0800

    updated text

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,3 @@
 require("./lib/social"); //Do not delete
 
 
-
-var flkty = new Flickity('.slideshow', {
-	prevNextButtons: false,
-pagedots: false
-});
-
-var carouselStatus = document.querySelector('.carousel-status');
-
-function updateStatus() {
-  var slideNumber = flkty.selectedIndex + 1;
-  carouselStatus.textContent = slideNumber + '/' + flkty.slides.length;
-}
-updateStatus();
-
-flkty.on( 'select', updateStatus );
